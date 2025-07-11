bb9837fd7c4af63783bfdd76fdb21f079f10a88e
AmanErOnGit <amank.secure4@gmail.com>
2025-07-11 17:06:23 +0530

    first commit

--- src/Components/Dashboard/AddUser.jsx
+++ src/Components/Dashboard/AddUser.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const AddUserModal = ({ onClose }) => {
+  
   const initialFields = [
     { name: 'empId', placeholder: 'Employee ID', required: true },
     { name: 'password', placeholder: 'Create Password', required: true },
