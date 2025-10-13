8d6544e7a763d7eb0044f530c696f3a0f3548678
itVpl <it@vpower-logistics.com>
2025-10-13 17:18:41 +0530

    invoice and checkinvoice

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -73,7 +73,8 @@ const menuItems = [
   { name: "Task Schedule", icon: BlueRevenueStatic, whiteIcon: WhiteRevenueStatic, path: "/TaskScheduling" },
   { name: "CMT Dept Report", icon: BlueRevenueStatic, whiteIcon: WhiteRevenueStatic, path: "/CmtDeptReport" },
   { name: "Sales Dept Report", icon: BlueRevenueStatic, whiteIcon: WhiteRevenueStatic, path: "/SalesDeptReport" },
-
+{ name: "Invoices", icon: BlueRevenueStatic, whiteIcon: WhiteRevenueStatic, path: "/Invoices" },
+{ name: "Check Invoice", icon: BlueRevenueStatic, whiteIcon: WhiteRevenueStatic, path: "/CheckInvoice" },
 
 ];
 
