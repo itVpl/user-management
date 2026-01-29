82184083284eafe0e2ff1aeaca7fb7dc63196988
abhi-vlog01 <collegework883@gmail.com>
2026-01-29 23:25:57 +0530

    modify dashboard

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -349,7 +349,7 @@ const Topbar = () => {
   }, []);
 
   return (
-    <div className="fixed top-6 left-[355px] right-22 h-16 z-50 flex items-center">
+    <div className="fixed top-6 left-[335px] right-15 h-16 z-50 flex items-center">
       <div className="w-full bg-white border border-gray-300 rounded-xl px-6 py-3 flex items-center justify-between">
         {/* Left Section - Checklist Stepper Icons */}
         <div className="flex items-center gap-2">
