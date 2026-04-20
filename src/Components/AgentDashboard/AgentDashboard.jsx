f83dbb367214018c04e785ba4dbeded7989c13d2
abhi-vlog01 <collegework883@gmail.com>
2026-04-20 15:15:27 +0530

    modify thali visibility on page

--- src/Components/AgentDashboard/AgentDashboard.jsx
+++ src/Components/AgentDashboard/AgentDashboard.jsx
@@ -11,7 +11,6 @@ import { IoCall } from "react-icons/io5";
 import CallFollowUpsDashboardCard from './CallFollowUpsDashboardCard';
 import API_CONFIG from '../../config/api';
 import MyAssignedTruckersWidget from '../Dashboard/MyAssignedTruckersWidget';
-import DinnerThaliDashboardCard from '../DinnerThali/DinnerThaliDashboardCard';
 // import firstIcon from "../../assets/Icon.svg"
 
 const Dashboard = () => {
@@ -498,7 +497,6 @@ const Dashboard = () => {
   });
   return (
     <div className="p-6 bg-white min-h-screen">
-      <DinnerThaliDashboardCard />
       {department === 'CMT' ? (
         // --- CMT Dashboard (current layout) ---
         <>
